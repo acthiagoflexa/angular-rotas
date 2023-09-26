190ce75723a206391aca62aaaccc4eeb99cb6adc
acthiagoflexa <thiagoflexa_@hotmail.com>
2023-09-26 13:40:44 -0300

    finished

--- src/app/pages/portifolio/card/card.component.ts
+++ src/app/pages/portifolio/card/card.component.ts
@@ -1,10 +1,35 @@
 import { Component } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
 
 @Component({
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrls: ['./card.component.css']
 })
-export class CardComponent {
 
+//http://localhost:4200/portfolio/{1}
+export class CardComponent {
+  constructor(
+    private activeRoute: ActivatedRoute,
+    private navegador: Router
+    ){
+    this.activeRoute.params.subscribe(
+      res => console.log(res)
+    )
+
+    this.activeRoute.firstChild?.params.subscribe(
+      res => console.log(res)
+    )
+
+    //http://localhost:4200/portfolio/1?{name=thiago&token=123}
+    this.activeRoute.queryParams.subscribe(
+      res=> console.log(res)
+    )
+  }
+
+  ngOnit(): void{
+    // setInterval(()=>{
+    //   this.navegador.navigate(['/'])
+    // },5000)
+  }
 }
